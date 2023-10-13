d6ae3183f20589190905513093c7d122933d2fb5
David Marques <davidmarques@Davids-MacBook-Air.local>
2023-10-13 23:13:45 +0100

    Add interceptor to add api key to the headers

--- app/src/main/java/com/example/thedogbreeds/services/RetrofitInstance.kt
+++ app/src/main/java/com/example/thedogbreeds/services/RetrofitInstance.kt
@@ -1,18 +1,35 @@
 package com.example.thedogbreeds.services
 
 import com.example.thedogbreeds.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
+    private val client: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(ApiKeyHeaderInterceptor())
+        .build()
+
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
     }
 
     val dogBreedService: DogBreedService by lazy {
         retrofit.create(DogBreedService::class.java)
+    }
+}
+
+class ApiKeyHeaderInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request().newBuilder()
+            .addHeader("x-api-key", BuildConfig.API_KEY)
+            .build()
+        return chain.proceed(request)
     }
 }
